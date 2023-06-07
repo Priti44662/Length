df1c350a6d6d5865764d7b698b8937c8fc2db2aa
Priti44662 <135731060+Priti44662@users.noreply.github.com>
2023-06-07 10:18:02 +0530

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,10 @@
+public class Length
+	{
+	public static void main(String[] args)
+	{
+     String text="mithu singh";
+		{
+			System.out.println(" The length of the text string is : " + text.length());
+     }
+}
+}
